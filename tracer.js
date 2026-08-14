422a217f6e04fd39febe7385fc0453ea1ba80521
Kai Takami <kai@domu.ai>
2026-08-13 18:01:19 -0700

    fix(security): remediate dependency vulnerabilities

--- tracer.js
+++ tracer.js
@@ -1,8 +1,7 @@
 const opentelemetry = require('@opentelemetry/api');
-const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const { resourceFromAttributes } = require('@opentelemetry/resources');
+const { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } = require('@opentelemetry/semantic-conventions');
 const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
@@ -18,21 +17,10 @@ const {
 module.exports = (serviceName) => {
   if (JAMBONES_OTEL_ENABLED) {
     const {version} = require('./package.json');
-    const provider = new NodeTracerProvider({
-      resource: new Resource({
-        [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
-        [SemanticResourceAttributes.SERVICE_VERSION]: version,
-      }),
-    });
-
     const exporters = [];
 
     if (OTEL_EXPORTER_JAEGER_AGENT_HOST  || OTEL_EXPORTER_JAEGER_ENDPOINT) {
       exporters.push(new JaegerExporter());
-    }
-
-    if (OTEL_EXPORTER_ZIPKIN_URL) {
-      exporters.push(new ZipkinExporter({url:OTEL_EXPORTER_ZIPKIN_URL}));
     }
 
     if (OTEL_EXPORTER_ZIPKIN_URL) {
@@ -45,8 +33,8 @@ module.exports = (serviceName) => {
       }));
     }
 
-    exporters.forEach((element) => {
-      provider.addSpanProcessor(new BatchSpanProcessor(element, {
+    const spanProcessors = exporters.map((exporter) =>
+      new BatchSpanProcessor(exporter, {
         // The maximum queue size. After the size is reached spans are dropped.
         maxQueueSize: 100,
         // The maximum batch size of every export. It must be smaller or equal to maxQueueSize.
@@ -55,20 +43,19 @@ module.exports = (serviceName) => {
         scheduledDelayMillis: 500,
         // How long the export can run before it is cancelled
         exportTimeoutMillis: 30000,
-      }));
+      })
+    );
+    const provider = new NodeTracerProvider({
+      resource: resourceFromAttributes({
+        [ATTR_SERVICE_NAME]: serviceName,
+        [ATTR_SERVICE_VERSION]: version,
+      }),
+      spanProcessors,
     });
 
     // Initialize the OpenTelemetry APIs to use the NodeTracerProvider bindings
     provider.register();
-    registerInstrumentations({
-      instrumentations: [
-        //new HttpInstrumentation(),
-        //new ExpressInstrumentation(),
-        //new PinoInstrumentation()
-      ],
-    });
   }
 
   return opentelemetry.trace.getTracer(serviceName);
 };
-
